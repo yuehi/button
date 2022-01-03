a3ff6c4b73e83b4671e070f406b7f13771f15681
YUHEI <76258455+yuehi@users.noreply.github.com>
2022-01-03 22:56:14 +0900

    button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { PrimaryButton } from "./component/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./component/atoms/button/SecondaryButton";
+import { SearchInput } from "./component/molecules/SearchInput";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +8,8 @@ export default function App() {
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
