1671c13e7e55303928a6e544d303487d39c90237
YUHEI <76258455+yuehi@users.noreply.github.com>
2022-01-04 23:35:06 +0900

    buttom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./component/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./component/atoms/button/SecondaryButton";
 import { SearchInput } from "./component/molecules/SearchInput";
+import { UserCard } from "./component/organisums/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "じゃけえ",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
