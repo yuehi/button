e13aeba0daaa570f88a12920d745bd9ee7073c5e
YUHEI <76258455+yuehi@users.noreply.github.com>
2022-01-11 00:09:07 +0900

    button

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,18 +1,14 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Top } from "../component/pages/Top";
-import { Users } from "../component/pages/Users";
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Top />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-      </Switch>
+    <Switch>
+      <Route exact path="/">
+        < />
+      </Route>
+    </Switch>
+    
     </BrowserRouter>
   );
 };
