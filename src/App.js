855fe67c841ac5d2b6977a46685b2f8e11493a13
YUHEI <76258455+yuehi@users.noreply.github.com>
2022-01-10 21:28:31 +0900

    button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./component/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./component/atoms/button/SecondaryButton";
-import { SearchInput } from "./component/molecules/SearchInput";
-import { UserCard } from "./component/organisums/user/UserCard";
-import { DefaultLayout } from "./component/templetes/DefaultLayout";
-import { HeaderOnly } from "./component/templetes/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 const user = {
@@ -19,15 +13,5 @@ const user = {
 };
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
