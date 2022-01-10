e6bf1b4cef02d0038dd3e1818bfbac814257ee52
YUHEI <76258455+yuehi@users.noreply.github.com>
2022-01-11 00:51:51 +0900

    button

--- src/component/pages/Users.jsx
+++ src/component/pages/Users.jsx
@@ -1,13 +1,45 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisums/user/UserCard";
+
+const users = [...Array(10).key()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけえ${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "12345@example.com",
+    phone: "090-1111-2222",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArae>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArae>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArae = styled.div`
+  padding-top: 40px;
+  width: 100px;
+  display: grid;
+  grid-auto-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Top } from "../component/pages/Top";
+import { Users } from "../component/pages/Users";
+import { DefaultLayout } from "../component/templetes/DefaultLayout";
+import { HeaderOnly } from "../component/templetes/HeaderOnly";
 
 export const Router = () => {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        < />
-      </Route>
-    </Switch>
-    
+      <Switch>
+        <Route exact path="/">
+          <DefaultLayout>
+            <Top />
+          </DefaultLayout>
+        </Route>
+        <Route exact path="/users">
+          <HeaderOnly>
+            <Users />
+          </HeaderOnly>
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
