245f225dd3209343203348e5fa817074bf109198
YUHEI <76258455+yuehi@users.noreply.github.com>
2022-01-19 23:46:36 +0900

    button

--- src/component/pages/Top.jsx
+++ src/component/pages/Top.jsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { useHistory } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
   const history = useHistory();
-  const onClickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onClickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  const { setUserInfo } = useContext(UserContext);
+
+  const onClickAdmin = () => setUserInfo({ isAdmin: true });
+  history.push("/users");
+  const onClickGeneral = () => setUserInfo({ isAdmin: false });
+  history.push("/users");
 
   return (
     <SContainer>
